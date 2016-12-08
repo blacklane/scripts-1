a9c40972488801d0587fbc3f488e1dc8083aa680
sofiavistas <sofia.vistas@gmail.com>
2016-12-08 16:36:17 +0100

    Update report path to work on jenkins workspace

--- androidTest/android-acceptance-tests.sh
+++ androidTest/android-acceptance-tests.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 date +%b-%dT%H.%M > timestamp.out
 
-sites="$HOME"/Documents/Android/pepper
-
-timestamp="$(cat "$sites"/timestamp.out)"
-result_folder="$sites"/results
+timestamp="$(cat timestamp.out)"
+result_folder=results
 destdir="$result_folder/Results-$timestamp"
 
 mkdir -p "$destdir"
@@ -12,37 +10,37 @@ echo "Directory created: ${destdir##*/}"
 
 echo "Starting Booking Tests"
 ./gradlew spoon -PspoonClassName=com.blacklane.passenger.test.acceptance.BookingTests
-cp -r "$sites"/app/build/spoon "$destdir"/BookingTests
+cp -r app/build/spoon "$destdir"/BookingTests
 echo "Results saved to BookingTests folder"
 
 echo "Starting Registration Tests"
 ./gradlew spoon -PspoonClassName=com.blacklane.passenger.test.acceptance.RegistrationTests
-cp -r "$sites"/app/build/spoon "$destdir"/RegistrationTests
+cp -r app/build/spoon "$destdir"/RegistrationTests
 echo "Results saved to RegistrationTests folder"
 
 echo "Starting Login Tests"
 ./gradlew spoon -PspoonClassName=com.blacklane.passenger.test.acceptance.LoginTests
-cp -r "$sites"/app/build/spoon "$destdir"/LoginTests
+cp -r app/build/spoon "$destdir"/LoginTests
 echo "Results saved to LoginTests folder"
 
 echo "Starting Rides Tests"
 ./gradlew spoon -PspoonClassName=com.blacklane.passenger.test.acceptance.RidesTests
-cp -r "$sites"/app/build/spoon "$destdir"/RidesTests
+cp -r app/build/spoon "$destdir"/RidesTests
 echo "Results saved to RidesTests folder"
 
 echo "Starting Profile Tests"
 ./gradlew spoon -PspoonClassName=com.blacklane.passenger.test.acceptance.ProfileTests
-cp -r "$sites"/app/build/spoon "$destdir"/ProfileTests
+cp -r app/build/spoon "$destdir"/ProfileTests
 echo "Results saved to ProfileTests folder"
 
 echo "Starting Manage Payments Tests"
 ./gradlew spoon -PspoonClassName=com.blacklane.passenger.test.acceptance.ManagePaymentsTests
-cp -r "$sites"/app/build/spoon "$destdir"/ManagePaymentsTests
+cp -r app/build/spoon "$destdir"/ManagePaymentsTests
 echo "Results saved to ManagePaymentsTests folder"
 
 echo "Starting Forgot Password Tests"
 ./gradlew spoon -PspoonClassName=com.blacklane.passenger.test.acceptance.ForgotPasswordTests
-cp -r "$sites"/app/build/spoon "$destdir"/ForgotPasswordTests
+cp -r app/build/spoon "$destdir"/ForgotPasswordTests
 echo "Results saved to ForgotPasswordTests folder"
 
 
